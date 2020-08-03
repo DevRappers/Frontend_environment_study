3a1ec05d1b221f8b1ef76bbfd15123581733e550
DevRappers <devrappers@gmail.com>
2020-08-03 13:19:00 +0900

    webpack loader

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -1,16 +1,24 @@
 const path = require("path");
 
 /*
-* entry : 시작점
-* output : 저장위치 
-* */
+ * entry : 시작점
+ * output : 저장위치
+ * */
 module.exports = {
-    mode: "development",
-    entry:{
-        main: "./src/app.js"
-    },
-    output:{
-        path: path.resolve("./dist"),
-        filename: '[name].js'
-    }
-}
+  mode: "development",
+  entry: {
+    main: "./src/app.js"
+  },
+  output: {
+    path: path.resolve("./dist"),
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [path.resolve("./my-webpack-loader.js")]
+      }
+    ]
+  }
+};
